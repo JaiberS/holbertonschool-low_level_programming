355b96e08ab74342c23f955217687922d1e0ab8b
JaiberS <758@holbertonschool.com>
2019-02-06 18:41:25 +0000

    Betty fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,16 @@
 */
 int main(void)
 {
-  char a;  
+char a;
 int j;
- long int b;
- long long int e;
- float i;
- printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
- printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(j));
- printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(b));
- printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(e));
- printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(i));
+long int b;
+long long int e;
+float i;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(j));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(e));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(i));
 return (0);
 }
