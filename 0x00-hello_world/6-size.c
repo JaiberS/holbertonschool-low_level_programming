9b170f166c01bcce82e443a54b188c55da3d8195
JaiberS <758@holbertonschool.com>
2019-02-06 18:38:14 +0000

    Size of variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int j;
  long int b;
  long long int e;
  float i;
- printf("The size of a char is: %lu byte(s)\n",(unsigned long)sizeof(a));
- printf("The size of an int is: %lu byte(s)\n",(unsigned long)sizeof(j));
- printf("The size of a long int is: %lu byte(s)\n",(unsigned long)sizeof(b));
- printf("The size of a long long int is: %lu byte(s)\n",(unsigned long)sizeof(e));
- printf("The size of a float is: %lu byte(s)\n",(unsigned long)sizeof(i));
+ printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
+ printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(j));
+ printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(b));
+ printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(e));
+ printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(i));
 return (0);
 }
