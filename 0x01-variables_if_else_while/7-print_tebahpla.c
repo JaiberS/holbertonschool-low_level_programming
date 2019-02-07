a57b7616fff7f168db1c0e9ccdb190d777d8a0cf
JaiberS <758@holbertonschool.com>
2019-02-07 16:05:02 +0000

    Alphabetto in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prind alphabet
+ * Return: always returns 0
+*/
+
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		alphabet--;
+	}
+		 putchar('\n');
+	return (0);
+}
