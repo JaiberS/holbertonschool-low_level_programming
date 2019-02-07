694bd53a828689788f150a464390d41d96181f92
JaiberS <758@holbertonschool.com>
2019-02-07 17:56:33 +0000

    combination of numbers until 99 with no numbers repeated

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Return: always returns 0
+*/
+
+int main(void)
+{
+	int alphabet = 0;
+	int number = 0;
+	int total = 0;
+	int fnd = 0;
+	int snd = 0;
+
+	while (fnd < 10)
+	{
+		while (snd < 10)
+		{
+			while (alphabet < 10)
+			{
+				while (number < 10)
+				{
+					if (snd < number)
+					{
+						putchar(fnd + '0');
+						putchar(snd + '0');
+						putchar(' ');
+						putchar(alphabet + '0');
+						putchar(number + '0');
+						if (total < 4499)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+						total++;
+					}
+					number++;
+				}
+				alphabet++;
+				number = 0;
+			}
+			snd++;
+			alphabet = 0;
+			number = 0;
+		}
+		fnd++;
+		snd = 0;
+		alphabet = 0;
+		number = 0;
+	}
+		 putchar('\n');
+	return (0);
+}
