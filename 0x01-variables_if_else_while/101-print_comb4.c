2925b9060c6908ff8a8815b8502e1164d27a1148
JaiberS <758@holbertonschool.com>
2019-02-07 17:25:58 +0000

    numbers until 999 with no repetition on digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Return: always returns 0
+*/
+
+int main(void)
+{
+	int alphabet = 0;
+	int number = 0;
+	int rddigit = 0;
+	int total = 0;
+
+	while (alphabet < 10)
+	{
+		while (number < 10)
+		{
+			while (rddigit <10)
+			{
+				if (number < rddigit && alphabet < number)
+				{
+					putchar(alphabet + '0');
+					putchar(number + '0');
+					putchar(rddigit + '0');
+					if (total < 789)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				total++;
+				rddigit++;
+			}
+			number++;
+			rddigit = 0;
+		}
+		alphabet++;
+		number = 0;
+		rddigit = 0;
+	}
+		 putchar('\n');
+	return (0);
+}
