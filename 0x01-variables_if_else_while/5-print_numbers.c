33f62b3c005bf0de516a3757e3d12463a83cbbd5
JaiberS <758@holbertonschool.com>
2019-02-07 15:52:58 +0000

    first 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Return: always returns 0
+*/
+
+int main(void)
+{
+	int dec = 0;
+
+	while (dec < 10)
+	{
+		printf("%i", dec);
+		dec++;
+	}
+	printf("\n");
+	return (0);
+}
