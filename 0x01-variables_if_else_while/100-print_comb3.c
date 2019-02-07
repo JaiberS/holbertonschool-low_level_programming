29728c1175e89965d7c4c366871e4ee70c68229b
JaiberS <758@holbertonschool.com>
2019-02-07 17:00:22 +0000

    numbers to 99 with no repetitions of digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Return: always returns 0
+*/
+
+int main(void)
+{
+	int alphabet = 0;
+	int number = 0;
+	int total = 0;
+
+	while (alphabet < 10)
+	{
+		while (number < 10)
+		{
+			if(alphabet < number)
+			{
+				putchar(alphabet + '0');
+				putchar(number + '0');
+				if (total < 89)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			total++;
+			number++;
+		}
+		alphabet++;
+		number = 0;
+	}
+		 putchar('\n');
+	return (0);
+}
