e9f93f72a669b81d58df3f761963229e12edf826
JaiberS <758@holbertonschool.com>
2019-02-12 12:49:14 +0000

    Betty Fix 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,9 @@
 #include "holberton.h"
 /**
- * main - prind alphabet
- * Return: always returns 0
+ * print_sign - prints the sign of the number
+ * @n: The number to be checked
+ *
+ * Return: 1 positive 0 = 0 -1 negative
 */
 
 int print_sign(int n)
@@ -9,17 +11,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
-        else if (n == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
-		return(0);
+		return (0);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
-	return(0);
+	return (0);
 }
