33f163c28baf064bb787b77fe0205956cc247f1e
JaiberS <758@holbertonschool.com>
2019-02-12 12:46:49 +0000

    Betty Fix 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,20 @@
 #include "holberton.h"
 /**
- * main - prind alphabet
- * Return: always returns 0
+ * _isalpha - checks if the input is a letter
+ *@j: The input to be checked
+ *
+ * Return: return 1 true 0 false
 */
 
 int  _isalpha(int j)
 {
-	if (j>= 'a' && j<= 'z')
+	if (j >= 'a' && j <= 'z')
 	{
-		return(1);
+		return (1);
 	}
-        else if (j>= 'A' && j<= 'Z')
+	else if (j >= 'A' && j <= 'Z')
 	{
-		return(1);
+		return (1);
 	}
-	return(0);
+	return (0);
 }
