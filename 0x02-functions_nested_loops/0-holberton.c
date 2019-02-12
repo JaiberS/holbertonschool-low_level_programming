6b313f3d6d7f43a0d29c79424a64240d8296e48a
JaiberS <758@holbertonschool.com>
2019-02-12 14:13:11 +0000

    Betty Fix 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -11,10 +11,10 @@
 int main(void)
 {
 	/*char Holb[11] = "Holberton";*/
-	char Holb[10] = {'H','o','l','b','e','r','t','o','n','\n'};
+	char Holb[10] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\n'};
 	int i = 0;
 
-	for(;i<=9;i++)
+	for (; i <= 9; i++)
 	{
 		_putchar(Holb[i]);
 	}
