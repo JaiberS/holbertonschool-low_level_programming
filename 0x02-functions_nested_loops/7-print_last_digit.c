e846853fae7e59d7acb5d1f200916d4d0c9d1485
JaiberS <758@holbertonschool.com>
2019-02-12 12:53:28 +0000

    Betty fix 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
 /**
- * main - prind alphabet
- * Return: always returns 0
+ * print_last_digit - prints the last digit of a number.
+ * @j: The number to get the last digit.
+ *
+ * Return: Returns the value of the last digit
 */
 
 int print_last_digit(int j)
 {
 	if (j < 0)
-        {
-                j = -j;
+	{
+		j = -j;
 	}
 	j = j % 10;
 	_putchar(j + '0');
-	return(j);
+	return (j);
 }
