4e182f1c0bc417e3663e9821380b764b8b3e0aee
JaiberS <758@holbertonschool.com>
2019-02-12 13:02:36 +0000

    Betty Fix 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,13 @@
 #include "holberton.h"
 /**
- * main - prind alphabet
- * Return: always returns 0
+ * add - add two numbers
+ *@fn: First number to be added
+ *@sn: Seconf number to be added
+ *
+ * Return: The result of the add of the numbers
 */
 
 int add(int fn, int sn)
 {
-	return(fn + sn);
+	return (fn + sn);
 }
