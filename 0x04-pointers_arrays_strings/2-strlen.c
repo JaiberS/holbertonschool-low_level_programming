32b8e386a5e4679d49a9c973f0ef5e84235e313d
JaiberS <758@holbertonschool.com>
2019-02-19 12:27:56 +0000

    String lenght

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * swap_int - Resets variable to 98 value
+ * @a: Value to be changed
+ * @b:
+ *
+ */
+
+int _strlen(char *s)
+{
+	int i=0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return(i);
+}
