e0696ff777ff522f19c8eab9c35fdcebec389b4c
JaiberS <758@holbertonschool.com>
2019-02-19 12:41:28 +0000

    Prints string in rev

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints the string array in reverse
+ * @s: array to print
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+		i++;
+	for (; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
