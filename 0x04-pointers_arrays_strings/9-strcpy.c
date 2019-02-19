afa0255dcc29e8994731a6cc28b2c25e63d214cf
JaiberS <758@holbertonschool.com>
2019-02-19 17:06:32 +0000

    Weird Fix

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -13,11 +13,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i] != -1)
+	while (src[i] != 0)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = src[i];
+	dest[i + 1] = src[i + 1];
 	return (dest);
 }
