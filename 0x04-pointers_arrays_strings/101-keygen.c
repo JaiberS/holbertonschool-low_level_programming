8a5ba35073f6874bb97dfaad955010b76306e086
alejagargom <791@holbertonschool.com>
2019-02-20 01:53:42 +0000

    Decode password

---
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - decode 101-crackme password
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int j = 0, s, r = 127;
+
+	srand(time(NULL));
+	s = rand() % r;
+	while (j <= 2772 - r)
+	{
+		j = s + j;
+		_putchar(s);
+	}
+	s = 2772 - j;
+	_putchar(s);
+	return (0);
+}
