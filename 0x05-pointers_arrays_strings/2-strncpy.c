d9e0be834ed887022ce921fe45d234e89a77addb
JaiberS <758@holbertonschool.com>
2019-02-21 18:08:48 +0000

    Betty Fix 2

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -3,30 +3,21 @@
 #include <stdlib.h>
 
 /**
- * _strcat -  function that concatenates two strings.
+ * _strncpy -  function that concatenates two strings until n chars.
  *@dest: string to add the src string.
  *@src: string to be added to the dest string.
+ *@n: limit of strings to be added.
  *
  * Return: pointer to the resulting string dest.
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, k = 0;
-	char j[999];
+	int k = 0;
 
-	for (; dest[i] != '\0'; i++)
-		;
-	for (; src[k] != '\0'; k++)
-	{
-		if (k < n)
-		{
-			j[k] = src[k];
-			dest[k] = j[k];
-		}
-		i++;
-	}
-	if (k < n)
+	for (; src[k] != '\0' && k < n; k++)
+		dest[k] = src[k];
+	for (; k < n; k++)
 		dest[k] = '\0';
 	return (dest);
 }
