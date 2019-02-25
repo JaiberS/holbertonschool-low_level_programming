ad1796da4c0ec73d53640ae3bc1373b6d89d3cdc
JaiberS <758@holbertonschool.com>
2019-02-25 20:08:23 +0000

    Print the sum of the diags of a nxn matrix

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _memset -  fills memory with a constant byte
@@ -11,16 +12,18 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i = 0, j = 0, i2 = 0, j2 = 0;
+	int i = 0, j = 0, i2 = 0, j2 = 0, k = 0;
 
 	for (; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
 			if (j == i)
-				i2 = i2 + **a[i][j];
-			else
-				j2 = j2 + **a[i][j];
+				i2 = i2 + a[k];
+			if (j + i == size - 1)
+				j2 = j2 + a[k];
+			k++;
 		}
 	}
+	printf("%i, %i\n", i2,j2);
 }
