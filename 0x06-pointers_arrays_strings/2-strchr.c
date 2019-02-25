f239a9613500c4a3345571a02bf67084a2429f7a
JaiberS <758@holbertonschool.com>
2019-02-25 18:28:17 +0000

    Betty Fix 2

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -2,27 +2,27 @@
 #include <stdio.h>
 
 /**
- * _memset -  fills memory with a constant byte
- * @s: memory to be filled
- * @b: constant byte
- * @n: until n
+ * _strchr -  locates a character in a string.
+ * @s: string.
+ * @c: characgter to be located.
  *
- *Return: Returns a pointer to the memory area s
- */
+ *Return: pointer to the first occurrence of the character c in the string s,
+ *or NULL if the character is not found.
+*/
 
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0, m0 = 0;
 	char **s2 = &s;
 
-	for (;s[i]; i++)
+	for (; s[i]; i++)
 	{
 		if (s[i] == c)
 		{
 			*s2 = (s + i);
 			break;
 		}
-		if (s[i + 1]== '\0')
+		if (s[i + 1] == '\0')
 		{
 			m0 = 1;
 			break;
@@ -30,9 +30,6 @@ char *_strchr(char *s, char c)
 	}
 	if (m0 == 0)
 		return (*s2);
-	else
-	{
-		*s2 = (s + i);
-		return (*s2);
-	}
+	*s2 = (s + i);
+	return (*s2);
 }
