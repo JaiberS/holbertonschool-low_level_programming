bbee15c72d2f4487d720a91ecb0838c73b17673a
JaiberS <758@holbertonschool.com>
2019-02-25 20:14:19 +0000

    Betty Fix 9

--- 0x06-pointers_arrays_strings/9-set_string.c
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -1,12 +1,10 @@
 #include "holberton.h"
 
 /**
- * _memset -  fills memory with a constant byte
- * @s: memory to be filled
- * @b: constant byte
- * @n: until n
+ * set_string - sets the value of a pointer to a char.
+ * @s: pointer
+ * @to: char
  *
- *Return: Returns a pointer to the memory area s
  */
 
 void set_string(char **s, char *to)
