f5da2ea64588c39f6e8717be06f5ca860d3a176a
JaiberS <758@holbertonschool.com>
2019-03-02 17:18:48 +0000

    Betty Fix 3

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -1,7 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - multiplies two numbers
+ * @argc: counter of the inputs
+ * @argv: the inputs in string
+ *
+ * Return: 1 if there are no two numbers only else 0
+ */
+
+int main(int argc, char *argv[])
 {
 	if (argc == 3)
 		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
