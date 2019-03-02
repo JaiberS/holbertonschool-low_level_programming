ce813fc6ddb98dd3b3d28962c61346c3d3670c15
JaiberS <758@holbertonschool.com>
2019-03-02 17:33:43 +0000

    Betty Fix 100

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - calculates the required change
+ *@argc: counter of the inputs
+ *@argv: the inputs in string
+ *
+ *Return: the number of coins for the change
+ */
+
+int main(int argc, char *argv[])
 {
 	int i, j = 0;
 
-	if(argc != 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -19,25 +27,25 @@ int main (int argc, char *argv[])
 			j++;
 		}
 		else if (i % 10 == 0)
-                {
-                        i = i - 10;
-                        j++;
-                }
+		{
+			i = i - 10;
+			j++;
+		}
 		else if (i % 5 == 0)
-                {
-                        i = i - 5;
-                        j++;
-                }
+		{
+			i = i - 5;
+			j++;
+		}
 		else if (i % 2 == 0)
-                {
-                        i = i - 2;
-                        j++;
-                }
+		{
+			i = i - 2;
+			j++;
+		}
 		else if (i % 1 == 0)
-                {
-                        i = i - 1;
-                        j++;
-                }
+		{
+			i = i - 1;
+			j++;
+		}
 	}
 	printf("%i\n", j);
 	return (0);
