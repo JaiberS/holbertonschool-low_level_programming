eb318cb2a31e9146b2258cde3d93eb78844426b4
JaiberS <758@holbertonschool.com>
2019-03-02 17:10:56 +0000

    Betty Fix 0

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - test argv
+ * @argc: counter of the inputs
+ * @argv: the inputs in string
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
 {
 	int i, j;
 
 	i = argc;
 	j = i;
 	i = j;
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
