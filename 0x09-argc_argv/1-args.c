186751332bdfdb3e88fa451b39cbfe1d61a3dc36
JaiberS <758@holbertonschool.com>
2019-03-02 17:16:04 +0000

    Betty Fix 1

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - test argc
+ * @argc: counter of the inputs
+ * @argv: the inputs in string
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
 {
 	char *i, *j;
 
 	i = argv[0];
 	j = i;
 	i = j;
-	printf("%i\n",argc - 1);
+	printf("%i\n", argc - 1);
 	return (0);
 }
