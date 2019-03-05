2b73d780c6ebc1bd43d2b85da2082ff45f721f2b
JaiberS <758@holbertonschool.com>
2019-03-05 19:36:09 +0000

    Null Fix 2 with empty

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -11,15 +11,15 @@ char *_strdup(char *str)
 	int i = 0, j = 0;
 	char *p;
 
+	if (str == NULL)
+		return (0);
 	for (; str[i] != '\0'; i++)
 		;
 	i++;
-	if (str == NULL)
-		return (0);
 /*	printf("%i",i);*/
 	p = malloc(sizeof(char) * i);
 	if (p == NULL)
-		return (0);
+		return (p);
 	for (; j < i; j++)
 		p[j] = str[j];
 	return (p);
--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -9,22 +9,28 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *p = s1;
+	char *p;
 	int i = 0, j = 0;
 
-	for (; s1[i] != '\0'; i++)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (; s1[i] != '\0' && s1 != NULL; i++)
 		;
-	for (i = i - 1; s2[j] != '\0'; i++, j++)
+	if (s1 != NULL && s2 != NULL)
+		i = i - 1;
+	for (; s2[j] != '\0' && s2 != NULL; i++, j++)
 		;
-	p = malloc(sizeof(char) * i);
+	p = malloc(sizeof(char) * (i + 1));
 	if (p == NULL)
 		return (0);
 	j = 0;
-	for (; s1[j] != '\0'; j++)
+	for (; s1[j] != '\0' && s1 != NULL; j++)
 		p[j] = s1[j];
 	i = 0;
-	for (; s2[i] != '\0'; i++, j++)
+	for (; s2[i] != '\0' && s2 !=NULL; i++, j++)
 		p[j] = s2[i];
-	p[j] = s2[i];
+	p[j] = '\0';
 	return (p);
 }
