5e3178de293331cb03e03485be18ec0ae565fa37
JaiberS <758@holbertonschool.com>
2019-03-05 18:33:08 +0000

    Betty Fix 1

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 
 /**
- * str_concat -
+ * str_concat -concatenates two strings.
+ * @s1: First string
+ * @s2: second string
  *
- * Return: 
+ * Return: NULL on failure
  */
 char *str_concat(char *s1, char *s2)
 {
+/*	char *p;
+	int i = 0, j = 0;*/
 
+	printf("%lu - %lu", sizeof(s1),sizeof(s2));
+	return (28);
 }
