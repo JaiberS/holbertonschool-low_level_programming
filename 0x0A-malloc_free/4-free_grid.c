afedad9336477e4ab3dd53acc09b7331f80781ee
JaiberS <758@holbertonschool.com>
2019-03-05 23:46:36 +0000

    Check requirements

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -10,10 +10,12 @@ void free_grid(int **grid, int height)
 {
 	int i, *currentgrid;
 
+	if (height == 0 || grid == NULL)
+		return;
 	for (; i < height; i++)
 	{
 		currentgrid = grid[i];
 		free(currentgrid);
 	}
-	free (grid);
+	free(grid);
 }
