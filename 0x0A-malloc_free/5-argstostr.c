7364ff82dc2f4c6a8c875a1709855de062f1b2a9
JaiberS <758@holbertonschool.com>
2019-03-05 21:57:24 +0000

    concatenates a string

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,11 +1,33 @@
 #include "holberton.h"
 
 /**
- * argstostr -
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argc
+ * @av: argv
  *
- * Return: 
+ * Return:  pointer to a new string, or NULL if it fails
  */
 char *argstostr(int ac, char **av)
 {
+	int i = 0, j = 0, k = 0;
+	char *p;
 
+	if (ac == 0 || av == NULL)
+		return (0);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, k++)
+			;
+	}
+	p = malloc(sizeof(char) * (k + 1));
+	for (i = 0, k = 0; i < ac; i++)
+        {
+                for (j = 0; av[i][j] != '\0'; j++, k++)
+                        p[k] = av[i][j];
+		p[k] = '\n';
+/*		printf("%i",k);*/
+		k++;
+        }
+	p[k] = '\0';
+	return (p);
 }
