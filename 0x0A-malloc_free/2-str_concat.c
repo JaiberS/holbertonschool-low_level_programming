96f72221eefc1b93354bcee7d27030852fdd74e9
JaiberS <758@holbertonschool.com>
2019-03-05 19:39:36 +0000

    Betty Fix 2

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -16,21 +16,19 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (; s1[i] != '\0' && s1 != NULL; i++)
+	for (; s1[i] != '\0'; i++)
 		;
-	if (s1 != NULL && s2 != NULL)
-		i = i - 1;
-	for (; s2[j] != '\0' && s2 != NULL; i++, j++)
+	for (i = i - 1; s2[j] != '\0'; i++, j++)
 		;
 	p = malloc(sizeof(char) * (i + 1));
 	if (p == NULL)
 		return (0);
 	j = 0;
-	for (; s1[j] != '\0' && s1 != NULL; j++)
+	for (; s1[j] != '\0'; j++)
 		p[j] = s1[j];
 	i = 0;
-	for (; s2[i] != '\0' && s2 !=NULL; i++, j++)
+	for (; s2[i] != '\0'; i++, j++)
 		p[j] = s2[i];
-	p[j] = '\0';
+	p[j] = s2[i];
 	return (p);
 }
