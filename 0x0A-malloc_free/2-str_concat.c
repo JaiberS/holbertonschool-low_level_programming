5243e44276749499129f963630cee5ecb726bc44
JaiberS <758@holbertonschool.com>
2019-03-05 18:45:55 +0000

    Concatenate two strings

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -9,9 +9,22 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-/*	char *p;
-	int i = 0, j = 0;*/
+	char *p = s1;
+	int i = 0, j = 0;
 
-	printf("%lu - %lu", sizeof(s1),sizeof(s2));
-	return (28);
+	for (; s1[i] != '\0'; i++)
+		;
+	for (i = i - 1; s2[j] != '\0'; i++, j++)
+		;
+	p = malloc(sizeof(char) * i);
+	if (p == NULL)
+		return (0);
+	j = 0;
+	for (; s1[j] != '\0'; j++)
+		p[j] = s1[j];
+	i = 0;
+	for (; s2[i] != '\0'; i++, j++)
+		p[j] = s2[i];
+	p[j] = s2[i];
+	return (p);
 }
