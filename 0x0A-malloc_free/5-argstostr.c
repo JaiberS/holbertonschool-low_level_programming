78c82c77e5d02fa2f2c3da012875bbffc530114a
JaiberS <758@holbertonschool.com>
2019-03-05 22:11:29 +0000

    Weird Fix

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -19,7 +19,7 @@ char *argstostr(int ac, char **av)
 		for (j = 0; av[i][j] != '\0'; j++, k++)
 			;
 	}
-	p = malloc(sizeof(char) * (k + 1));
+	p = malloc(sizeof(char) * (k + 10));
 	if (p == NULL)
 		return (0);
 	for (i = 0, k = 0; i < ac; i++)
