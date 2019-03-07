d0144e1664d42c32588da20cbe5411efbf3e0235
JaiberS <758@holbertonschool.com>
2019-03-07 18:20:11 +0000

    Logix Fix 2

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -3,8 +3,8 @@
 /**
  * _realloc -  reallocates a memory block
  * @ptr: is a pointer to the memory previously allocated
- * @old_size is the size, in bytes, of the allocated space for ptr
- * @new_size is the new size, in bytes of the new memory block
+ * @old_size: is the size, in bytes, of the allocated space for ptr
+ * @new_size: is the new size, in bytes of the new memory block
  *
  * Return: pointer to the new allocation with the values from prev
  */
@@ -18,7 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return(NULL);
+		return (NULL);
 	}
 	p = malloc(new_size);
 	if (p == NULL)
@@ -27,7 +27,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-/*	for (; ptr[i] != '\0'; i++)
-	p[i] = ptr[i];*/
+/*
+ * for (; ptr[i] != '\0'; i++)
+ *	p[i] = ptr[i];
+ */
 	return (p);
 }
