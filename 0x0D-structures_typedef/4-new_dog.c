57fe60b39a1f91fbfb829e637bb03b19a61f0a45
JaiberS <758@holbertonschool.com>
2019-03-11 13:12:59 +0000

    Makes new dogs

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,11 +1,23 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * new_dog -
+ * new_dog - creates a new dog.
+ * @name:
+ * @age:
+ * @owner:
  *
- * Return: 
+ * Return: Return NULL if the function fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *my_dog;
 
+	my_dog = malloc(sizeof(dog_t));
+	if (my_dog == NULL)
+		return (NULL);
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
+	return (my_dog);
 }
---
+++ 0x0D-structures_typedef/5-free_dog.c5-free_dog.c
@@ -0,0 +1,11 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs.
+ *
+ * Return: 
+ */
+void free_dog(dog_t *d)
+{
+
+}
