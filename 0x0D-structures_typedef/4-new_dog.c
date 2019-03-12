8e6f5f750a32ee74e5ae5120cadddda4e2c841a6
JaiberS <758@holbertonschool.com>
2019-03-12 00:43:34 +0000

    New alloc for every char *

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,34 @@
 #include "dog.h"
 #include <stdlib.h>
 
+char *_strdup(char *str);
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ *@str: string to be copied
+ *
+ * Return: pointer to a new string, NULL if str = NULL
+ */
+char *_strdup(char *str)
+{
+	int i = 0, j = 0;
+	char *p;
+
+	if (str == NULL)
+		return (0);
+	for (; str[i] != '\0'; i++)
+		;
+	i++;
+/*      printf("%i",i);*/
+	p = malloc(sizeof(char) * i);
+	if (p == NULL)
+		return (p);
+	for (; j < i; j++)
+		p[j] = str[j];
+	return (p);
+}
+
+
+
 /**
  * new_dog - creates a new dog.
  * @name: name of the dog
@@ -16,8 +44,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 		return (NULL);
-	my_dog->name = name;
+	my_dog->name = _strdup(name);
+	if (my_dog->name != NULL)
+		my_dog->name = name;
+	else
+	{
+		free(my_dog);
+		return (NULL);
+	}
 	my_dog->age = age;
-	my_dog->owner = owner;
+	my_dog->owner = _strdup(owner);
+	if (my_dog->name != NULL)
+		my_dog->owner = owner;
+	else
+	{
+		free(my_dog);
+		return (NULL);
+	}
 	return (my_dog);
 }
