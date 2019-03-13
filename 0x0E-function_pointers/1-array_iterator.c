62dfc18f9ac8a91e94b855e0844907aee6a8f600
JaiberS <758@holbertonschool.com>
2019-03-13 18:18:39 +0000

    Check array on 1

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	for (; i < size && size > 0 && action; i++)
+	for (; i < size && size > 0 && action && array; i++)
 	{
 		action(array[i]);
 	}
