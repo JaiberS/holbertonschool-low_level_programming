e6284f2106be74d0e4915830c8d701224d25e3be
JaiberS <758@holbertonschool.com>
2019-03-13 14:17:50 +0000

    function with arrays

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,11 +1,18 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator -
+ * array_iterator - executes a function as a parameter on each Array element.
+ * @array: a
+ * @size: s
+ * @action: ac
  *
- * Return: 
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i = 0;
 
+	for (; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
