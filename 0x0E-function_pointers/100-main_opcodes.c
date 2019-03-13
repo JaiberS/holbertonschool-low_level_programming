cbc674ea76b62abe3f68644fdaf0328564862713
JaiberS <758@holbertonschool.com>
2019-03-13 20:07:41 +0000

    Checking the check

--- 0x0E-function_pointers/100-main_opcodes.c
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -7,13 +7,14 @@
  *
  * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
+/*	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
-	}
+		}*/
 	if (atoi(argv[1]) < 0)
 	{
 		printf("Error\n");
