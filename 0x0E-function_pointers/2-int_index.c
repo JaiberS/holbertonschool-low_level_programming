b6126a4e2278a396df6d70263094a326227c9687
JaiberS <758@holbertonschool.com>
2019-03-13 14:36:53 +0000

    Check index of the return

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,11 +1,22 @@
 #include "function_pointers.h"
 
 /**
- * int_index -
+ * int_index - searches for an integer.
+ * @array: Array of integer elements
+ * @size: size of the array
+ * @cmp: functions to compare
  *
- * Return: 
+ * Return:  the index of the first element for which the cmp.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i = 0, j = 0;
 
+	for (; i < size; i++)
+	{
+		j = cmp(array[i]);
+		if (j != 0)
+			break;
+	}
+	return (i);
 }
