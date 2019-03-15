f6927ee867565914fdae14724894e4518c64def9
JaiberS <758@holbertonschool.com>
2019-03-15 19:37:41 +0000

    adds all arguments

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -1,11 +1,26 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all -
+ * sum_them_all - sum of all its parameters.
  *
- * Return: 
+ * Return: sum of all its parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list valist;
+	unsigned int sum = 0.0;
+	unsigned int i;
 
+	/* initialize valist for num number of arguments */
+	va_start(valist, n);
+
+	/* access all the arguments assigned to valist */
+	for (i = 0; i < n; i++) {
+		sum += va_arg(valist, int);
+	}
+
+	/* clean memory reserved for valist */
+	va_end(valist);
+
+	return sum;
 }
