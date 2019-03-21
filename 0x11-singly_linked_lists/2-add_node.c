edf5234eb8ae0ecef53df3ab4e9605ffdf76d1e3
JaiberS <758@holbertonschool.com>
2019-03-21 20:36:26 +0000

    [Feature] Add a new node

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -1,11 +1,29 @@
 #include "lists.h"
 
 /**
- * add_node -
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: address of the struct
+ * @str: string to be added
  *
- * Return: 
+ * Return: the address of the new element, or NULL if it failed
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *siguiente;
+	int i = 0;
 
+	siguiente = malloc(sizeof(list_t));
+	if (siguiente == NULL)
+	{
+		free(siguiente);
+		return (0);
+	}
+	siguiente->str = strdup(str);
+	while (str[i] != '\0')
+		i++;
+	siguiente->len = i;
+	siguiente->next = *head;
+	*head = siguiente;
+	return (*head);
 }
