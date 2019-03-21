cdecb492ab195778eca378bafa86646d5cd9081b
JaiberS <758@holbertonschool.com>
2019-03-21 23:56:01 +0000

    Prints a sentence from a turtle

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void wisdomturtle(void) __attribute__((constructor));
+
+/**
+ * wisdomturtle - prints the turtle wisdom
+ *
+ */
+
+void wisdomturtle(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
