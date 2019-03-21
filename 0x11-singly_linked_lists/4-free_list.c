00385ed8c417516c43f46efdc047cd1d74d75cce
JaiberS <758@holbertonschool.com>
2019-03-21 23:49:52 +0000

    Free all

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
 /**
- * free_list -
+ * free_list - frees a list_t list.
+ * @head: the list;
  *
- * Return: 
  */
 void free_list(list_t *head)
 {
+	list_t *iterate;
 
+	iterate = head;
+	while (iterate)
+	{
+		free(iterate->str);
+		free(iterate);
+		iterate = iterate->next;
+	}
 }
