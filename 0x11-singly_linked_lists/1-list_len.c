cb51209c3fc02bc65f35082a4f176fbc445a9e57
JaiberS <758@holbertonschool.com>
2019-03-21 20:07:37 +0000

    returns number of nodes

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -1,11 +1,22 @@
 #include "lists.h"
 
+
+
 /**
- * list_len -
+ * list_len -  returns the number of elements in a linked list_t list.
+ * @h: the list
  *
- * Return: 
+ * Return: the number of nodes
  */
 size_t list_len(const list_t *h)
 {
+	size_t i = 0;
 
+	if (h == NULL)
+		return (0);
+	for (; h != NULL; h = h->next)
+	{
+		i++;
+	}
+	return (i);
 }
