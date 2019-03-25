4b7896324e20749458a281f5d15f905755fbdde7
JaiberS <758@holbertonschool.com>
2019-03-25 14:33:22 +0000

    Frees the listint

--- 0x12-more_singly_linked_lists/4-free_listint.c
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 
 /**
- * free_listint -
+ * free_listint -  frees a listint_t list.
+ * @head: the list
  *
- * Return: 
  */
 void free_listint(listint_t *head)
 {
-
+	for (; head != NULL; head = head->next)
+		free(head);
 }
