6db5ee7705704a37607bd35ce693a3fbb25dc5d6
JaiberS <758@holbertonschool.com>
2019-03-26 15:53:05 +0000

    Fixed logic or to and

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (0);
 	}
 	head2 = *head;
-	for (; i < idx || head2 != NULL; head2 = head2->next, i++)
+	for (; i < idx && head2 != NULL; head2 = head2->next, i++)
 		;
 	if (head2 != NULL)
 	{
