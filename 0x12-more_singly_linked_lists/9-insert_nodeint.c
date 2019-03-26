81dbb4b45212a57aacde143831431a03bd8ecb6b
JaiberS <758@holbertonschool.com>
2019-03-26 21:11:58 +0000

    Changed new to head on almost last return

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -46,7 +46,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->n = n;
 		new->next = NULL;
 		*head = new;
-		return (new);
+		return (*head);
 	}
 	return (NULL);
 
