461687deea49b699ae13df02d8f2f619f9eab31c
JaiberS <758@holbertonschool.com>
2019-03-26 15:45:56 +0000

    Fixed logic or to and

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,18 +17,18 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (head2 == NULL)
 		return (-1);
 	for (; i < index
-		     || head2 == NULL; i++,
+		     && head2 != NULL; i++,
 		     anterior = head2, head2 = head2->next)
 		;
 	if (head2 != NULL)
 	{
-			free(head2);
-			head2 = head2->next;
-			if (i != 0)
-				anterior->next = head2;
-			else
-				*head = head2;
-			return (1);
+		free(head2);
+		head2 = head2->next;
+		if (i != 0)
+			anterior->next = head2;
+		else
+			*head = head2;
+		return (1);
 	}
 	return (-1);
 }
