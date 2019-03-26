c35410696b1f514b1eb09ba7d8ba0e30cfee226e
JaiberS <758@holbertonschool.com>
2019-03-26 20:55:08 +0000

    Logic Fix checking null

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ant;
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (0);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
@@ -41,13 +43,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		return (new);
 	}
-	if (idx == 0)
-	{
-		new->n = n;
-		new->next = NULL;
-		*head = new;
-		return (new);
-	}
-	return (NULL);
-
+	new->n = n;
+	new->next = NULL;
+	*head = new;
+	return (new);
 }
