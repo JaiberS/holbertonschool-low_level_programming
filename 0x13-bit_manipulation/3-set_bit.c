77563e5554bbf296db7f958728c1607da1b848c5
JaiberS <758@holbertonschool.com>
2019-03-28 23:06:00 +0000

    Sets a bit at index

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
 
 /**
- * set_bit -
+ * set_bit -  sets the value of a bit to 1 at a given index.
+ * @n: number in decimal
+ * @index:  index, starting from 0 of the bit you want to get
  *
- * Return: 
+ * Return: the value of the bit at index index or -1 if an error occured
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int cmp = 1;
 
+	if (index > 32)
+		return (-1);
+	for (; index > 0; index--)
+		cmp = cmp << 1;
+	*n = *n | cmp;
+	return (1);
 }
