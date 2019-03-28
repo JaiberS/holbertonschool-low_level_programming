02954f52cdc9b190d45a2d0b5003bf0afa112d92
JaiberS <758@holbertonschool.com>
2019-03-28 22:40:17 +0000

    Get index birt

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,11 +1,43 @@
 #include "holberton.h"
 
 /**
- * get_bit -
+ * get_bit -  returns the value of a bit at a given index.
+ * @n: number in decimal
+ * @index:  index, starting from 0 of the bit you want to get
  *
- * Return: 
+ * Return: the value of the bit at index index or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int numbern = 0, number = 0, receiv = 1, i = 0, k = 0;
+	int lastbit = 0;
 
+	for (receiv = 1; numbern + receiv <= n ; i++)
+	{
+		number = receiv;
+		receiv *= 2;
+	}
+	if (index > i - 1)
+		return (-1);
+	if (n == 0)
+		lastbit = 0;
+	else
+		lastbit = 1;
+	numbern += number;
+	for (; i - 1 != index; i--)
+	{
+		for (receiv = 1, k = 0; numbern + receiv <= n && k < i; k++)
+		{
+			number = receiv;
+			receiv *= 2;
+		}
+		if (k != i - 1 && numbern != n)
+			lastbit = 0;
+		else
+		{
+			lastbit = 1;
+			numbern += number;
+		}
+	}
+	return (lastbit);
 }
