41ca7d8a5c780bfedc060aaa29ed0f967531db1e
JaiberS <758@holbertonschool.com>
2019-03-28 23:43:26 +0000

    checks the number of bits required to flip from one to another

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,22 @@
 #include "holberton.h"
 
 /**
- * flip_bits -
+ * flip_bits - check number of bits you would need to flip
+ * @n: first number
+ * @m: second nuber
  *
- * Return: 
+ * Return: the number of bits you would need to flip to get from one to another
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int cmp1 = 1, cmp2 = 1, counter = 0;
 
+	for (; n != 0 || m != 0;  n = n >> 1,  m = m >> 1)
+	{
+		cmp1 = n & 1;
+		cmp2 = m & 1;
+		if (cmp1 != cmp2)
+			counter++;
+	}
+	return (counter);
 }
