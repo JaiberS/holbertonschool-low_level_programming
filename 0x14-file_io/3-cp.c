f5976be34cb05f6f67415f53e23e44aa4c716722
JaiberS <758@holbertonschool.com>
2019-04-02 21:24:59 +0000

    Fix logic and errors

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -10,43 +10,39 @@
 
 int main(int argc, char *argv[])
 {
-	int fd;
-	ssize_t conf;
+	int fdf, fdt;
+	ssize_t confr, confw;
 	char buf[1024];
 
 	if (argc != 3)
-	{		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
+	fdf = open(argv[1], O_RDONLY);
+	if (fdf == -1)
+		dprintf(2, "Error: Can't read from %s\n", argv[1]),
+			exit(98);
+	fdt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fdt == -1)
+		dprintf(2, "Error: Can't write to %s\n", argv[2]),
+			exit(99);
+	do {
+		confr = read(fdf, buf, 1024);
+		if (confr == -1)
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]),
+				exit(98);
+		confw = write(fdt, buf, confr);
+		if (confw == -1)
+			dprintf(2, "Error: Can't write to %s\n", argv[2]),
+				exit(99);
+	} while (confr != '\0');
+	confr = close(fdt);
+	confw = close(fdf);
+	if (confr == -1 || confw == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[0]);
-		exit(98);
-	}
-	conf = read(fd, buf, 1024);
-	if (conf == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[0]);
-		exit(98);
-	}
-	close(fd);
-	fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd == -1)
-	{
-		dprintf(2, "Error: Can't write to file %s\n", argv[0]);
-		exit(99);
-	}
-	conf = write(fd, buf, conf);
-	if (conf == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	conf = close(fd);
-	if (conf == -1)
-	{
-		dprintf(2, "Error: Can't close fd %i\n", fd);
+		if (confr == -1)
+			dprintf(2, "Error: Can't close fd %i\n", fdt);
+		if (confw == -1)
+			dprintf(2, "Error: Can't close fd %i\n", fdf);
 		exit(100);
 	}
 	return (0);
