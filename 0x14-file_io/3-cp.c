05ba53004a001ebba95797327ace062450842ac1
JaiberS <758@holbertonschool.com>
2019-04-02 20:46:43 +0000

    copies one file to another

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,46 +1,42 @@
 #include "holberton.h"
 
+/**
+ * main - copies one file to another
+ * @argc: number of inputs
+ * @argv: inputs in string
+ *
+ * Return: 0
+ */
 
-
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int fd;
 	ssize_t conf;
 	char buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	{		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-/* read */
-
-        fd = open(argv[1], O_RDONLY);
-
-        if (fd == -1)
-        {
-                dprintf(2, "Error: Can't read from file %s\n", argv[0]);
-                exit(98);
-        }
-        conf = read(fd, buf, 1024);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[0]);
+		exit(98);
+	}
+	conf = read(fd, buf, 1024);
 	if (conf == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[0]);
 		exit(98);
 	}
-        close(fd);
-
-/* write */
-
-	fd = open(argv[2],O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
+	close(fd);
+	fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd == -1)
 	{
 		dprintf(2, "Error: Can't write to file %s\n", argv[0]);
 		exit(99);
 	}
-
 	conf = write(fd, buf, conf);
 	if (conf == -1)
 	{
