9bf5c3bc8626e20f71023e8021bb835f997739ed
JaiberS <758@holbertonschool.com>
2019-04-02 21:58:47 +0000

    Read First

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -18,24 +18,22 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
 			exit(97);
 	fdf = open(argv[1], O_RDONLY);
-	if (fdf == -1)
+	fdt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	confr = read(fdf, buf, 1024);
+	if (confr == -1)
 		dprintf(STDERR_FILENO,
 			"Error: Can't read from file %s\n", argv[1]), exit(98);
-	fdt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fdt == -1)
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", argv[2]),	exit(99);
 	while (confr != '\0')
-	{	confr = read(fdf, buf, 1024);
-		if (confr == -1)
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]),
-				exit(98);
-		confw = write(fdt, buf, confr);
+	{	confw = write(fdt, buf, confr);
 		if (confw == -1 || confw != confr)
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]),
 				exit(99);
+		confr = read(fdf, buf, 1024);
+		if (confr == -1)
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]),
+				exit(98);
 	}
 	confr = close(fdt);
 	confw = close(fdf);
