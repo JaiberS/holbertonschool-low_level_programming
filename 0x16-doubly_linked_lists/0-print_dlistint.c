8fa0d2a68ac0a421debede65cad687a649fda39d
Lucia Rodriguez <774@holbertonschool.com>
2019-05-04 20:12:14 +0000

    [Fixes] Logic Fix

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -11,15 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 
-	if (h->prev == NULL)
-	{
-		for (; h != NULL; i++, h = h->next)
-			printf("%i\n", h->n);
-	}
-	else if (h->next == NULL)
-	{
-		for (; h != NULL; i++, h = h->prev)
-			printf("%i\n", h->n);
-	}
+	for (; h != NULL; i++, h = h->next)
+		printf("%i\n", h->n);
 	return (i);
 }
