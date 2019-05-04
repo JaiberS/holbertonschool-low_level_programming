baca4e8570ff255ed0946c36648baf21582baa3a
Lucia Rodriguez <774@holbertonschool.com>
2019-05-04 20:34:31 +0000

    [Feature] returns number of nodes

--- 0x16-doubly_linked_lists/1-dlistint_len.c
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 
 /**
- * dlistint_len -
+ * dliistint_len - return number of nodes in the list
+ * @h: the list
  *
- * Return: 
+ * Return: number of nodes
  */
+
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t i = 0;
 
+	for (; h != NULL; i++, h = h->next)
+		;
+	return (i);
 }
