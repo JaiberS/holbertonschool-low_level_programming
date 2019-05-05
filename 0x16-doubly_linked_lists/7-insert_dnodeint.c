0c0a0078ce2eb1c07872538ab32332fdd7bc47d0
JaiberS <758@holbertonschool.com>
2019-05-05 00:32:21 +0000

    [Fixes] Logix h null and prev

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -44,7 +44,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			new->next = *h;
 			new->prev = NULL;
-			(*h)->prev = new;
+			if (*h != NULL)
+				(*h)->prev = new;
 			*h = new;
 		}
 		return (new);
