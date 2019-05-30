19ebdf63d0fac4766eb1eda625aa1b25e389aab9
Haroldo <hvelez020@gmail.com>
2019-05-30 16:19:56 +0000

    [Feature] Create a hash table

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,11 +1,24 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create -
+ * hash_table_create - Creates a hash table
+ * @size: size of the array
  *
- * Return: 
+ * Return: the hash table address
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *hash_table = NULL;
 
+	hash_table = malloc(sizeof(hash_table) * 2);
+	if (hash_table == NULL)
+		return(NULL);
+	hash_table->array = malloc(sizeof(hash_node_t **) * size);
+	if (hash_table->array == NULL)
+	{
+		free (hash_table);
+		return(NULL);
+	}
+	hash_table->size = size;
+	return (hash_table);
 }
