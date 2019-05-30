b007a47419aea9053b2bccd1825682527e6f940b
Haroldo <hvelez020@gmail.com>
2019-05-30 22:38:04 +0000

    [fixes] Simple way to print

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -7,16 +7,12 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int sizeht = 0, i = 0, j = 0, size = 0, k = 0, ns, m0 = 0;
-	unsigned long int nsize = 0;
-	size_t ln = 0;
+	unsigned long int sizeht = 0, i = 0, m0 = 0;
 	hash_node_t *head;
-	char *string;
 
-	string = malloc(sizeof(string));
-	size = 8;
 	sizeht = ht->size;
-	for (i = 0; i < sizeht && string; i++)
+	printf("{");
+	for (i = 0; i < sizeht; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
@@ -24,74 +20,13 @@ void hash_table_print(const hash_table_t *ht)
 			for (; head != NULL; head = head->next)
 			{
 				if (m0 != 0)
-				{
-					if (j + 1 >= size)
-					{
-						string = realloc(string, size + 2);
-						size += 2;
-					}
-					string[j] = ',';
-					j++;
-					string[j] = ' ';
-					j++;
-				}
+					printf(", ");
 				else
 					m0 = 1;
-				if (j >= size)
-				{
-					string = realloc(string, size + 1);
-					size += 1;
-				}
-				string[j] = '\'';
-				j++;
-				ln = strlen(head->key);
-				ns = j + ln;
-				if (ns >= size)
-				{
-					nsize = ns - size;
-					string = realloc(string, size + nsize);
-					size += nsize;
-				}
-				if (ln != 0)
-				{
-					for (k = 0; (head->key)[k]; k++, j++)
-						string[j] = (head->key)[k];
-				}
-				if (j + 3 >= size)
-				{
-					string = realloc(string, size + 4);
-					size += 4;
-				}
-				string[j] = '\'';
-				j++;
-				string[j] = ':';
-				j++;
-				string[j] = ' ';
-				j++;
-				string[j] = '\'';
-				j++;
-				ln = strlen(head->value);
-				ns = j + ln;
-				if (ns >= size)
-				{
-					nsize = ns - size;
-					string = realloc(string, size + nsize);
-					size += nsize;
-				}
-				if (ln != 0)
-				{
-					for (k = 0; (head->value)[k]; k++, j++)
-						string[j] = (head->value)[k];
-				}
-				if (j >= size)
-				{
-					string = realloc(string, size + 1);
-					size += 1;
-				}
-				string[j] = '\'';
-				j++;
+				printf("\'%s\': \'%s", head->key, head->value);
+				printf("\'");
 			}
 		}
 	}
-	printf("{%s}\n", string);
+	printf("}\n");
 }
