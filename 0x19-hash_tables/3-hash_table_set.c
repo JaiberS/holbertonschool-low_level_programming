78c91d07ab30d96e74287b20eead7f5675093903
Haroldo <hvelez020@gmail.com>
2019-05-31 00:49:13 +0000

    [Fixes] No empty key

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,6 +14,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_table_t *ht2;
 	hash_node_t *head;
 
+	if (strcmp(key, "") == 0 || value == NULL || key == NULL)
+		return (0);
 	ht2 = ht;
 	size = ht2->size;
 	hash = key_index((const unsigned char *)key, size);
