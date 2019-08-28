f138584d01acf0ee62da41b5a1dbed29a22155c4
= <=>
2019-08-28 19:50:21 +0000

    [Feature] Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+	for (; i < size; i++){
+		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
